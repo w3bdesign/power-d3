06be657e5967f8df9a2e0f7978974468cda3be5d
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-10-16 01:26:54 +0200

    Update React and try out pie charts

--- src/components/d3/D3Component.js
+++ src/components/d3/D3Component.js
@@ -1,43 +1,99 @@
-import React, { useEffect, useRef } from 'react';
+// import React, { useEffect, useRef } from 'react';
 
-import * as d3 from 'd3';
+// import * as d3 from 'd3';
 
 // https://medium.com/@jeffbutsch/using-d3-in-react-with-hooks-4a6c61f1d102
+// https://www.freecodecamp.org/news/how-to-get-started-with-d3-and-react-c7da74a5bd9f/
+// https://www.freecodecamp.org/news/how-to-get-started-with-d3-and-react-c7da74a5bd9f/
 
-function D3Component({ data }) {
-  console.log('Props: ');
-  //console.log(data.viewer.homes[0].consumption.nodes[0]);
-  console.log(data);
+import React, { useEffect } from 'react';
+import * as d3 from 'd3';
 
-  const d3Container = useRef(null);
+const createPieChart = async () => {
+  const svg = d3.select('svg'),
+    width = svg.attr('width'),
+    height = svg.attr('height'),
+    radius = Math.min(width, height) / 2;
 
+  const g = svg
+    .append('g')
+    .attr('transform', `translate(${width / 2}, ${height / 2})`);
+
+  const color = d3.scaleOrdinal(['red', 'blue', 'yellow']);
+
+  const pie = d3.pie().value(function (d) {
+    return d.percent;
+  });
+
+  const path = d3
+    .arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
+
+  const label = d3
+    .arc()
+    .outerRadius(radius)
+    .innerRadius(radius - 200);
+
+  
+  const data = await d3.csv('http://localhost:3000/populations.csv');
+
+  const arc = g
+    .selectAll('.arc')
+    .data(pie(data))
+    .enter()
+    .append('g')
+    .attr('class', 'arc');
+
+  arc
+    .append('path')
+    .attr('d', path)
+    .attr('fill', function (d) {
+      return color(d.data.states);
+    });
+
+  arc
+    .append('text')
+    .attr('transform', function (d) {
+      return `translate(${label.centroid(d)})`;
+    })
+    .text(function (d) {
+      return d.data.states;
+    });
+
+  svg
+    .append('g')
+    .attr('transform', `translate(${width / 2 - 120},20)`)
+    .append('text')
+    .text('Test')
+    .attr('class', 'title');
+};
+
+export default function D3Component() {
   useEffect(() => {
-    if (data && d3Container.current) {
-      const svg = d3.select(d3Container.current);
-
-      // Bind D3 data
-      const update = svg.append('g').selectAll('text').data(data);
-
-      // Enter new D3 elements
-      update
-        .enter()
-        .append('text')
-        .attr('x', (d, i) => i * 25)
-        .attr('y', 40)
-        .style('font-size', 24)
-        .text((d) => d);
-
-      // Update existing D3 elements
-      update.attr('x', (d, i) => i * 40).text((d) => d);
-
-      // Remove old D3 elements
-      update.exit().remove();
-    }
-  }, [data]);
+    createPieChart();
+  }, []);
 
   return (
-    <svg className="d3-component" width={400} height={200} ref={d3Container} />
+    <div className="App">
+      <style>{`
+        .arc text {
+          font: 35px arial;
+          text-anchor: middle;
+          fill: #fff;
+          stroke: #000;
+        }
+
+        .arc path {
+          stroke: #fff;
+        }
+
+        .title {
+          fill: #fff;
+          font-weight: bold;
+        }
+      `}</style>
+      <svg width="700" height="700"></svg>
+    </div>
   );
 }
-
-export default D3Component;
