5f9975e93820bcde2182b3555d94d1824bd3ac6f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-10-19 03:05:09 +0200

    Show axis graph of cost and consumption

--- src/App.js
+++ src/App.js
@@ -16,27 +16,7 @@ function App() {
     <div className="App">
       {loading && <h1>Loading data ...</h1>}
       {error && <h1>Error loading data ...</h1>}
-
-      {data ? (
-        data.viewer.homes[0].consumption.nodes.map(
-          ({ consumption, cost, unitPrice, consumptionUnit }) => (
-            <>
-              <D3Component data={[1, 2, 3]} />
-              <div key={cost}>
-                <p>
-                  Consumption: {consumption} {consumptionUnit}
-                  <br />
-                  Cost: {cost} kr
-                  <br />
-                  Unit price: {unitPrice} kr
-                </p>
-              </div>
-            </>
-          )
-        )
-      ) : (
-        <h2>No data </h2>
-      )}
+      {data ? <D3Component data={data} /> : <h2>No data </h2>}
     </div>
   );
 }
--- src/components/d3/D3Component.js
+++ src/components/d3/D3Component.js
@@ -1,9 +1,73 @@
 import React, { useEffect } from 'react';
 
-export default function D3Component() {
-  useEffect(() => {
-    console.log('Render useEffect!');
-  }, []);
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+} from 'recharts';
 
-  return <div className="App">D3!</div>;
+const formatDate = (dateString) => {
+  const options = { day: 'numeric', month: 'long' };
+  return new Date(dateString).toLocaleDateString(undefined, options);
+  //return new Date(dateString).toLocaleDateString('nb-NO', options);
+};
+
+export default function D3Component({ data }) {
+  const consumptionData = data.viewer.homes[0].consumption.nodes;
+
+  const formattedDate = consumptionData.map((num) => {
+    return formatDate(num.to);
+    //console.log(num.to);
+    //return num * 2;
+  });
+
+  console.log(formattedDate);
+
+  /*const formattedForEach = consumptionData.forEach(
+    (test) => {
+      console.log(test);
+      if (test === 'to') {
+        console.log('toooo');
+      }
+    }*/
+
+  //console.log(formatDate(test))
+  //console.log(test)
+  //if(Object.keys(test))
+  //console.log(Object.keys(test))
+
+  /*{
+        if (Object.keys(test) === "to") {
+          console.log('To!');
+        }
+      }*/
+
+  //Object.keys(test) === 'to' && console.log('To!')
+  // );
+
+  /*useEffect(() => {
+    console.log('Render useEffect!');    
+    console.log(data.viewer.homes[0].consumption.nodes);
+  }, [data]);*/
+  // console.log(consumptionData);
+  //console.log(formattedDate);
+
+  return (
+    <div className="App">
+      <LineChart width={600} height={600} data={consumptionData}>
+        <Line type="monotone" dataKey="consumption" stroke="#8884d8" />
+        <Line type="monotone" dataKey="cost" stroke="#000" />
+
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="to" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+      </LineChart>
+    </div>
+  );
 }
