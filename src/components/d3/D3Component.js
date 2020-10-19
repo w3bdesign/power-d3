a1459b398fbafa283ff409811135e5b2a075a67f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-10-19 04:44:32 +0200

    Remove unused imports

--- src/components/d3/D3Component.js
+++ src/components/d3/D3Component.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import {
   LineChart,
@@ -16,37 +16,22 @@ const formatDate = (dateString) => {
 };
 
 export default function D3Component({ tibberData, settibberData }) {
-  console.log(tibberData);
-
   const consumptionData = tibberData.viewer.homes[0].consumption.nodes;
 
-  //console.log(consumptionData);
-
-  /*const formattedDate = consumptionData.map((num) => {
-    return formatDate(num.to);
-  });*/
-
-  /*const test = consumptionData.forEach((data, index) => {
+  const test = consumptionData.forEach((data, index) => {
     //if(data.to) { }
-    //console.log(index);
+    console.log(data);
     // console.log(data.to);
-    //consumptionData[index].to = "test";
-    console.log(consumptionData[index].to);
-  });*/
-
-  /*useEffect(() => {
-    console.log('Render useEffect!');    
-    console.log(data.viewer.homes[0].consumption.nodes);
-  }, [data]);*/
-  // console.log(consumptionData);
-  //console.log(formattedDate);
+    return formatDate(data.to);
+    //console.log(consumptionData[index].to);
+  });
+  console.log(test);
 
   return (
     <div className="App">
       <LineChart width={1000} height={600} data={consumptionData}>
         <Line type="monotone" dataKey="consumption" stroke="#8884d8" />
         <Line type="monotone" dataKey="cost" stroke="#000" />
-
         <CartesianGrid stroke="#ccc" />
         <XAxis dataKey="to" />
         <YAxis />
